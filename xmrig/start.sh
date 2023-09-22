7782098f0c63725b8e463d5e452d282d577fb6b0
Jose Garcia <54786476+Tauromachian@users.noreply.github.com>
2023-09-22 11:28:24 -0400

    Remove port

--- xmrig/start.sh
+++ xmrig/start.sh
@@ -5,4 +5,4 @@
 #./xmrig --donate-level 1 -o masari.herominers.com:10150 -u 5nV4GQAggfN9wuhYLwGYBrAB4zkJNd7ZsWPaDtG1RBoyKjKZei7ioqLSPKDnvuVSbaRaakgTs8UV7Gqqu4M12unoUP2tGdf -p X -a cn/half -k 
 
 #Monero
-./xmrig -o pool.supportxmr.com:443 -u 43AcBkpgoMfWAK9YZqAEmfDhju4hKynXiFu2GCWGMAQ2Nd7T5i62FAihgR7y7ZxrFUDnpE77B6ESqT9zEBUP7XigC9Wivmo
+./xmrig -o pool.supportxmr.com -u 43AcBkpgoMfWAK9YZqAEmfDhju4hKynXiFu2GCWGMAQ2Nd7T5i62FAihgR7y7ZxrFUDnpE77B6ESqT9zEBUP7XigC9Wivmo
